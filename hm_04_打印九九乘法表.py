82100a0c74d97638ba90ff1bcb0d8fce03c46978
lxz0503 <lxz_20081025@163.com>
2019-07-04 20:52:35 +0800

    bbbb

--- hm_04_打印九九乘法表.py
+++ hm_04_打印九九乘法表.py
@@ -1,4 +1,3 @@
-# 用9行9列的概念来实现
 row = 1
 while row <= 9:
     col = 1
@@ -6,11 +5,5 @@ while row <= 9:
         # 格式化输出，用-2d表示靠左对齐
         print("%d*%d=%d" % (row, col, row * col), end="\t")
         col += 1
-    print()  # 输出一个换行
+    print("")
     row += 1
-
-#
-for i in range(1, 10):
-    for j in range(1, i+1):
-        print("%d*%d=%d" % (j, i, j*i), end="\t")
-    print()
